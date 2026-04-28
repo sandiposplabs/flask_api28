daf0560d44ed109af25652861d889dba7cc9e3ef
Sandip Ugale <sandip.ugale@ecotechservices.com>
2026-04-28 16:31:53 +0530

    initial

--- api.py
+++ api.py
@@ -1,1 +1,14 @@
-##sss
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Flask API running on Azure 🚀"})
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
